ff03f48df71cc6e10a55df05425992c09b5dba62
changmu <2276479303@qq.com>
2015-07-28 14:15:26 +0800

    update

--- TCPServer.py
+++ TCPServer.py
@@ -9,7 +9,6 @@ print 'The server is ready to receive'
 while 1:
     connectionSocket, addr = serverSocket.accept()
     sentence = connectionSocket.recv(1024)
-    capitalizedSentence = sentence.upper()
     print 'TCP established from', addr
     connectionSocket.send('Hello World!')
     connectionSocket.close()
